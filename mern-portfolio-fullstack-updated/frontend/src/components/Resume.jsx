f5da46b025e067f4eedecd93086cc50c48bb613e
rohit9797 <rohitmeher513@gmail.com>
2025-09-18 18:43:57 +0530

    Prepare frontend for Netlify deployment

--- mern-portfolio-fullstack-updated/frontend/src/components/Resume.jsx
+++ mern-portfolio-fullstack-updated/frontend/src/components/Resume.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ArrowDownTrayIcon, EyeIcon } from '@heroicons/react/24/outline';
+import { AcademicCapIcon } from '@heroicons/react/24/outline';
 
 export default function Resume() {
   return (
